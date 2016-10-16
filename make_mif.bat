2ecbeca1b224492374938eece506d2e8e1769755
mshr-h <mshr-h@users.noreply.github.com>
2016-10-16 22:29:42 +0900

    add memory related files

---
+++ make_mif.bat
@@ -0,0 +1,8 @@
+tools\convert2mif.exe memory\memory_sw_A.txt fpga\memory_sw_A.mif
+tools\convert2mif.exe memory\memory_sw_B.txt fpga\memory_sw_B.mif
+tools\convert2mif.exe memory\memory_sw_C.txt fpga\memory_sw_C.mif
+tools\convert2mif.exe memory\memory_sw_D.txt fpga\memory_sw_D.mif
+tools\convert2mif.exe memory\memory_tb_A.txt fpga\memory_tb_A.mif
+tools\convert2mif.exe memory\memory_tb_B.txt fpga\memory_tb_B.mif
+tools\convert2mif.exe memory\memory_tb_C.txt fpga\memory_tb_C.mif
+tools\convert2mif.exe memory\memory_tb_D.txt fpga\memory_tb_D.mif
