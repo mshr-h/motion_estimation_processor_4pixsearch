7420a4d3b9c3c83d6f9b6de486e09132cd2f1db8
mshr-h <mshr-h@users.noreply.github.com>
2016-10-28 20:07:03 +0900

    fix memory_dual_port

--- testbench/memory_dual_port.v
+++ testbench/memory_dual_port.v
@@ -1,20 +1,20 @@
 `default_nettype none
 module memory_dual_port
-#(
-  parameter DWIDTH  = 6,
-  parameter AWIDTH  = 10,
-  parameter CONTENT = "./memory.txt"
-) (
+(
   input  wire [AWIDTH-1:0] address_a,
   input  wire [AWIDTH-1:0] address_b,
   input  wire              clock,
-  input  wire [7:0]        data_a,
-  input  wire [7:0]        data_b,
+  input  wire [DWIDTH-1:0] data_a,
+  input  wire [DWIDTH-1:0] data_b,
   input  wire              wren_a,
   input  wire              wren_b,
-  output reg  [7:0]        q_a,
-  output reg  [7:0]        q_b
+  output reg  [DWIDTH-1:0] q_a,
+  output reg  [DWIDTH-1:0] q_b
 );
+
+parameter DWIDTH  = 0;
+parameter AWIDTH  = 0;
+parameter CONTENT = "";
 
 reg [DWIDTH-1:0] core [0:2**AWIDTH-1];
 
