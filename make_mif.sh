4616aeaf6721e8ea5351050e9aa6da7664a165fe
mshr-h <mshr-h@users.noreply.github.com>
2016-10-28 19:10:18 +0900

    fix mif creator

--- make_mif.sh
+++ make_mif.sh
@@ -1,8 +1,8 @@
-go run tools/convert2mif.go tools/convert2mif.py memory/memory_sw_A.txt fpga/memory_sw_A.mif
-go run tools/convert2mif.go tools/convert2mif.py memory/memory_sw_B.txt fpga/memory_sw_B.mif
-go run tools/convert2mif.go tools/convert2mif.py memory/memory_sw_C.txt fpga/memory_sw_C.mif
-go run tools/convert2mif.go tools/convert2mif.py memory/memory_sw_D.txt fpga/memory_sw_D.mif
-go run tools/convert2mif.go tools/convert2mif.py memory/memory_tb_A.txt fpga/memory_tb_A.mif
-go run tools/convert2mif.go tools/convert2mif.py memory/memory_tb_B.txt fpga/memory_tb_B.mif
-go run tools/convert2mif.go tools/convert2mif.py memory/memory_tb_C.txt fpga/memory_tb_C.mif
-go run tools/convert2mif.go tools/convert2mif.py memory/memory_tb_D.txt fpga/memory_tb_D.mif
+go run tools/convert2mif.go memory/memory_sw_A.txt fpga/memory_sw_A.mif
+go run tools/convert2mif.go memory/memory_sw_B.txt fpga/memory_sw_B.mif
+go run tools/convert2mif.go memory/memory_sw_C.txt fpga/memory_sw_C.mif
+go run tools/convert2mif.go memory/memory_sw_D.txt fpga/memory_sw_D.mif
+go run tools/convert2mif.go memory/memory_tb_A.txt fpga/memory_tb_A.mif
+go run tools/convert2mif.go memory/memory_tb_B.txt fpga/memory_tb_B.mif
+go run tools/convert2mif.go memory/memory_tb_C.txt fpga/memory_tb_C.mif
+go run tools/convert2mif.go memory/memory_tb_D.txt fpga/memory_tb_D.mif
